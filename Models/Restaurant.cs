ed3a5cc66418d23d3a7a4acc841c48187b971a8c
bingyuyap <bingyu.yap.21@gmail.com>
2021-07-14 16:28:22 +0800

    Add ICollection for Item in Restaurant

--- Models/Restaurant.cs
+++ Models/Restaurant.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace DotnetMvc.Models
 {
@@ -7,5 +9,8 @@ namespace DotnetMvc.Models
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
+        
+        // A Restaurant object has many items
+        public ICollection<Item> Items { get; set; }
     }
 }
