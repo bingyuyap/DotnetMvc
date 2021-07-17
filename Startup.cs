6653986bdcec5ccf9cf8d9a8c6bc1d3ea25e9637
bingyuyap <bingyu.yap.21@gmail.com>
2021-07-17 17:36:08 +0800

    Add AutoMapper Configuration to Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -35,6 +35,9 @@ namespace DotnetMvc
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
+            
+            // Add AutoMapper configuration 
+            services.AddAutoMapper(typeof(Startup));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
